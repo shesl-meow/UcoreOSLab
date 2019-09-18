bc2ad09dcab24bb57442a855323f869a98076755
shesl-meow <shesonglin@bytedance.com>
2019-09-18 14:06:04 +0800

    update shell script

---
+++ Push.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if ! git diff-index --quiet HEAD --; then
+    echo "You have uncommit change."
+    exit 1;
+fi
+
+BRANCH_NAME=`git branch | grep \* | cut -d ' ' -f2`
+git pull origin $BRANCH_NAME
+
+if ! git diff-index --quiet HEAD --; then
+    echo "Please solve merge conflict."
+    exit 1;
+fi
+
+git push origin $BRANCH_NAME
+git push -f kancloud $BRANCH_NAME
+
+find . -type d -and -not -name ".*" -maxdepth 1 \
+    -exec cp -r {} ../Note/学校课程/操作系统/HOMEWORK/ \;
--- SUMMARY.md
+++ SUMMARY.md
@@ -1,7 +1,3 @@
-
 * [lab1](./lab1/README.md)
-
 	* [exe1](./lab1/exe1/README.md)
-
 	* [exe2](./lab1/exe2/README.md)
-
