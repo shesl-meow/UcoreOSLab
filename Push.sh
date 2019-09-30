f665b214d9f3c547638789ca0c18ccc023d71162
shesl-meow <shesonglin@bytedance.com>
2019-09-30 19:41:39 +0800

    update Push.sh

--- Push.sh
+++ Push.sh
@@ -16,5 +16,6 @@ fi
 git push origin $BRANCH_NAME
 git push -f kancloud $BRANCH_NAME
 
+rm ../Note/学校课程/操作系统/HOMEWORK/*
 find . -type d -and -not -name ".*" -maxdepth 1 \
     -exec cp -r {} ../Note/学校课程/操作系统/HOMEWORK/ \;
